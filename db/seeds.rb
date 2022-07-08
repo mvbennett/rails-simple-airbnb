99a2862a68328355abe11b3bd0ffe87e560b4c44
mikebennett <mvbennett9@gmail.com>
2022-07-08 18:47:04 +0900

    All actions working as necessary

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Very Cozy Closet in SoHo',
+  address: '5 blocks north of houston',
+  description: 'a whole 5 square feet wide! many cockroaches so we reuqire a pet deposit',
+  price_per_night: 200,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Awesome room in Nakagin tower',
+  address: 'Ginza no naka or something',
+  description: 'Actually a really cool space, might be falling apart and actively being destroyed but no worries',
+  price_per_night: 150,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Paris',
+  address: '10 Clifton Gardens Paris W9 1DT',
+  description: "Une belle sensation d'été pour cet appartement de jardin utilisant. Deux chambres doubles, un salon ouvert, une grande cuisine et une belle véranda",
+  price_per_night: 70,
+  number_of_guests: 2
+)
